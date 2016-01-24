d454166f706d6020b48f8fbe1f46f8ad7f43a61f
Istvan Szukacs <istvan@streambrightdata.com>
2016-01-24 15:12:34 +0100

    Adding deploy.sh and minor fixes

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+aws s3 sync . s3://www.streambrightdata.com/ \
+--exclude '*' \
+--include '*.html' \
+--include '*.css' \
+--include '*.gz' \
+--include '*.ico' \
+--include '*.eot' \
+--include '*.png' \
+--include '*.jpeg' \
+--include '*.jpg' \
+--include '*.js' \
+--include '*.json' \
+--include '*.svg' \
+--include '*.ttf' \
+--include '*.ttf' \
+--include '*.woff' \
+--include '*.xml' \
+--recursive \
+--acl public-read \
+--region us-east-1
